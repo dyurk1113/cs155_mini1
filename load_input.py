a350af21373a11130be7129272c0b71acba1dc16
Dom_Desktop\dyurk <dominicyurk@gmail.com>
2019-02-11 17:31:16 -0800

    Added more data dropout

--- load_input.py
+++ load_input.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import pandas
+import time
 
 data_folder = 'data'
 
@@ -22,11 +23,11 @@ def get_col_ignore():
 def load_data_file(file_name):
     data_file = os.path.join(os.getcwd(), data_folder, file_name)
     all_data = pandas.read_csv(data_file)
-    trim_data = all_data.drop(get_col_ignore(), axis=1)
-    return trim_data
+    all_data.drop(get_col_ignore(), axis=1, inplace=True)
+    return all_data
 
 
-def load_data(col_drop_rate = 0.0):
+def load_raw_data():
     train_data = load_data_file('train_2008.csv')
     train_out = train_data['target']
     train_out = train_out.astype(np.int8, copy=False)
@@ -34,6 +35,10 @@ def load_data(col_drop_rate = 0.0):
 
     test_data_2008 = load_test_data('2008')
     test_data_2012 = load_test_data('2012')
+    return train_data, train_out, test_data_2008, test_data_2012
+
+
+def process_inputs(train_data, test_data_2008, test_data_2012, col_drop_rate = 0.0, min_drop_thresh = 0.002):
     all_data = pandas.concat([train_data, test_data_2008, test_data_2012])
     drop_inds = []
     if col_drop_rate > 0:
@@ -42,19 +47,28 @@ def load_data(col_drop_rate = 0.0):
         drop_inds = gen_rand_inds(num_drops, num_cols)
         col_names = [all_data.columns[ind] for ind in drop_inds]
         all_data.drop(col_names, axis=1)
+    t = time.time()
     cat_data = categorize_data(all_data)
+    # print('Cat Time:', time.time()-t)
 
+    t = time.time()
     len_train, len_2008 = len(train_data), len(test_data_2008)
-    train_cat = cat_data.iloc[:len_train]
-    for col in train_cat:
-        if (train_cat[col] == 0).all():
-            del cat_data[col]
+    train_cat = cat_data.iloc[:len_train].values.astype(np.int8)
+    drop_thresh = len_train * min_drop_thresh
+    drop_cols = []
+    for col in range(len(cat_data.columns)):#train_cat:
+        num_hot = np.count_nonzero(train_cat[:, col])
+        if num_hot <= drop_thresh or num_hot >= (len_train - num_hot):
+            drop_cols.append(col)
+            # del cat_data[col]
+    cat_data.drop([cat_data.columns[c] for c in drop_cols], axis=1, inplace=True)
+    # print('Thresh Drop Time:', time.time() - t)
 
-    cat_data = cat_data.values.astype(np.int8, copy=False)
+    cat_data = cat_data.values.astype(np.int8)
     train_in = cat_data[:len_train]
     test_in_2008 = cat_data[len_train:len_train+len_2008]
     test_in_2012 = cat_data[len_train+len_2008:]
-    return train_in, train_out, test_in_2008, test_in_2012, drop_inds
+    return train_in, test_in_2008, test_in_2012, drop_inds
 
 
 def load_test_data(year):
@@ -68,6 +82,11 @@ def load_test_data(year):
 def categorize_data(data):
     for col in data:
         data[col][data[col] < 0] = -1
-    return pandas.get_dummies(data, columns=data.columns)
+    cat_data = pandas.get_dummies(data, columns=data.columns)#, drop_first=True)#, sparse=True)
+    neg_cols = [col for col in cat_data if '_-1' in col]
+    t = time.time()
+    cat_data.drop(neg_cols, axis=1, inplace=True)
+    # print('Drop Time:', time.time() - t)
+    return cat_data
 
 # load_data()
--- run_nn_model.py
+++ run_nn_model.py
@@ -2,52 +2,85 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.model_selection import StratifiedKFold
+import time
 
-from load_input import load_data
+from load_input import load_raw_data, process_inputs
 
-train_in, train_out, test_in_2008, test_in_2012, drop_cols = load_data()
+t = time.time()
+train_data, train_out, test_data_2008, test_data_2012 = load_raw_data()
+print('Raw Read Time:', time.time() - t)
+num_reps, drop_rate, min_drop_thresh = 1, 0.0, 0.002
+t = time.time()
+train_in, test_in_2008, test_in_2012, drop_cols = process_inputs(train_data, test_data_2008, test_data_2012, drop_rate, min_drop_thresh)
+print('Data Proc Time:', time.time() - t)
 # print('Loaded Data')
 
-num_cols = len(train_in[0])
-num_reps, drop_rate = 100, 0.2
-pres_accs, miss_accs = [[] for _  in range(num_cols)], [[] for _ in range(num_cols)]
-for rep in range(num_reps):
-    print('HERE 0')
-    train_in, train_out, test_in_2008, test_in_2012, drop_cols = load_data(drop_rate)
-    model = Sequential()
-    in_shape = np.shape(train_in)
-    model.add(Dense(100, input_dim=in_shape[1], activation='relu'))
-    model.add(Dropout(0.15)) #Determined from hyperparameter optimization
-    model.add(Dense(20, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
 
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    print('HERE 1')
-    fit = model.fit(train_in, train_out, batch_size=32, epochs=3, verbose=0)
-    print('HERE 2')
-    score = model.evaluate(train_in, train_out, verbose=0)
-    print(rep, 'Accuracy:', score[1])
-    for col in range(num_cols):
-        if col in drop_cols:
-            miss_accs[col].append(score[1])
-        else:
-            pres_accs[col].append(score[1])
+#No -1 Drop: 1192 cols, Train: 0.799919, Val: 0.778164
+#Do -1 Drop: 1140 cols, Train: 0.803611, Val: 0.780484
 
-results = [[col, np.mean(pres_accs[col]), np.std(pres_accs[col]),
-           np.mean(miss_accs[col]), np.std(miss_accs[col])] for col in range(num_cols)]
-np.savetxt('drop_col_res.csv', results, delimiter=',',
-           header='col,present mean,present std,dropped mean,dropped std',
-           comments='', fmt=['%d', '%.6f', '%.6f', '%.6f', '%.6f'])
+num_cols = len(train_data.columns)
+# pres_accs, miss_accs = [[] for _  in range(num_cols)], [[] for _ in range(num_cols)]
+# dts, train_accs, val_accs = [], [], []
+# for rep in range(num_reps):
+#     layer_size = 10 + 5*rep
+#     print(len(train_in[0]))
+#     skf = StratifiedKFold(n_splits=5)
+#     t_accs, v_accs = [], []
+#     for t_inds, v_inds in skf.split(train_in, train_out):
+model = Sequential()
+in_shape = np.shape(train_in)
+model.add(Dense(100, input_dim=in_shape[1], activation='relu'))
+model.add(Dropout(0.2)) #Determined from hyperparameter optimization
+model.add(Dense(20, activation='relu'))
+# model.add(Dense(5, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+        # print('HERE 1')
+        # fit = model.fit(train_in[t_inds], train_out[t_inds], batch_size=32, epochs=3, verbose=0)
+        # print('HERE 2')
+        # score = model.evaluate(train_in[t_inds], train_out[t_inds], verbose=0)
+        # t_accs.append(score[1])
+        # score = model.evaluate(train_in[v_inds], train_out[v_inds], verbose=0)
+        # v_accs.append(score[1])
+    #     print('FOLD')
+    # print('Train: %f, Val: %f' % (np.median(t_accs), np.median(v_accs)))
+    # dts.append(layer_size)
+    # train_accs.append(np.median(t_accs))
+    # val_accs.append(np.median(v_accs))
+# print('Rep %d, Acc %f' % (rep, val_accs[-1]))
+# print(train_accs)
+# print(val_accs)
+# plt.plot(dts, train_accs, label='Train')
+# plt.plot(dts, val_accs, label='Val')
+# plt.legend()
+# plt.show()
+# print('Accuracy:', score[1])
+#     print(rep, 'Accuracy:', score[1])
+#     for col in range(num_cols):
+#         if col in drop_cols:
+#             miss_accs[col].append(score[1])
+#         else:
+#             pres_accs[col].append(score[1])
+#
+# results = [[col, np.mean(pres_accs[col]), np.std(pres_accs[col]),
+#            np.mean(miss_accs[col]), np.std(miss_accs[col])] for col in range(num_cols)]
+# np.savetxt('drop_col_res.csv', results, delimiter=',',
+#            header='col,present mean,present std,dropped mean,dropped std',
+#            comments='', fmt=['%d', '%.6f', '%.6f', '%.6f', '%.6f'])
 #     accs.append(score[1])
 #
 # print(accs)
 # plt.plot(regs, accs)
 # plt.show()
 
-# out = model.predict(test_in_2008)
-# ids = np.reshape(np.arange(0, len(out), 1, dtype=np.int32), (len(out), 1))
-# out = np.concatenate((ids, out), axis=1)
-# np.savetxt('2008_out.csv', out, delimiter=',', header='id,target',
-#            fmt=['%d','%0.6f'], comments='')
+fit = model.fit(train_in, train_out, batch_size=8, epochs=7, verbose=2)
+out = model.predict(test_in_2008)
+ids = np.reshape(np.arange(0, len(out), 1, dtype=np.int32), (len(out), 1))
+out = np.concatenate((ids, out), axis=1)
+np.savetxt('2008_out.csv', out, delimiter=',', header='id,target',
+           fmt=['%d','%0.6f'], comments='')
